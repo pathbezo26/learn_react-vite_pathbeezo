fe4b1631efbd45037ecd49626fe613e9a2b47659
Nguyen Tan Phat <tanphatnguyen.tp@gmail.com>
2026-04-16 16:36:09 +0700

    video28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+
+  return (
+    <div>
+      <h1>Hello Nguyen Tan Phat!</h1>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +26,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
