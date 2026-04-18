a570dbb2bdba11f8f57537d891e45cbcf446492b
Nguyen Tan Phat <tanphatnguyen.tp@gmail.com>
2026-04-18 09:06:35 +0700

    DOM event

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,32 @@ import './components/todo/todo.css'
 import TodoData from './components/todo/TodoData'
 import TodoNew from './components/todo/TodoNew'
 import reactLogo from './assets/react.svg'
+
+
 function App() {
   const hobby = "Playing football"
   const inf = {
     country: "Viet Nam",
     address: "DeGi, GiaLai"
   }
+
+  const addNewTodo = (name) => {
+    alert(`func add new todo is called by ${name}`)
+  }
+
   return (
     <div className="todo-container">
 
       <div className="todo-title">My Todo</div>
 
-      <TodoNew />
+      <TodoNew
+        addNewTodo={addNewTodo}
+      />
+
       <TodoData
         hobby={hobby}
         inf={inf}
+
       />
 
       <div className='todo-image'>
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,9 +1,24 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(props)
+    const { addNewTodo } = props
+    //addNewTodo("Beezo")
+    const handleClick = () => {
+        alert("click add new todo is done")
+    }
+    const handleOnChange = (text) => {
+        console.log(">>> handleOnChange is excuted", text)
+    }
     return (
         <div className='TodoNew'>
-            <input type="text"></input>
-            <button>Add</button>
-        </div>
+            <input
+                type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            ></input>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
+        </div >
     )
 }
 
