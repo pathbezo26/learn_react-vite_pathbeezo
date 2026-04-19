595c686b0de76dcd770c117151289bdba5c33397
Nguyen Tan Phat <tanphatnguyen.tp@gmail.com>
2026-04-19 14:56:41 +0700

    "update"

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,14 +1,37 @@
 import './Header.css'
 import { Link } from 'react-router-dom';
+import './Header.css'
+import { NavLink } from 'react-router-dom'; // 1. Đổi Link thành NavLink
 
 const Header = () => {
     return (
         <ul>
-            <li><Link class="active" href="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/books">Books</Link></li>
+            {/* 2. Sử dụng NavLink và thuộc tính 'to' thay vì 'href' */}
+            <li>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/users"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                    Users
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/books"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                    Books
+                </NavLink>
+            </li>
         </ul>
-
     )
 }
 export default Header;
